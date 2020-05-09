e49a64c10eb0b225b8881d1bd96a90c98dd635fb
rubenpacheco <rubenpa.89@gmail.com>
2020-05-09 20:46:03 +0200

    prueba de cambios

---
+++ app/src/main/java/com/example/myplanninghome/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.myplanninghome;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+/* ejemplo verifica cambios git*/
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
